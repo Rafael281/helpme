4fd1f236e523356b741c1a0d6068a2af43fd9654
Rafael281 <143568684+Rafael281@users.noreply.github.com>
2023-10-13 09:59:30 -0300

    Adicionando um outro js

---
+++ usuario.js
@@ -0,0 +1,4 @@
+function toggleUserOptions() {
+  var userOptions = document.getElementById('userOptions');
+  userOptions.style.display = (userOptions.style.display === 'block') ? 'none' : 'block';
+}
